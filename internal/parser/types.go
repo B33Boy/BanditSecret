a7c6e96d1cd2028f9f1b9824ae64124e62c61536
B33Boy <abhip202@gmail.com>
2025-06-05 13:52:10 -0400

    moved types out of parser.go

---
+++ internal/parser/types.go
@@ -0,0 +1,15 @@
+package parser
+
+type CaptionMetadata struct {
+	VideoId     string
+	VideoTitle  string
+	Url         string
+	CaptionPath string
+}
+
+type CaptionParsed struct {
+	Id    string `json:"video_id"`
+	Start string `json:"start"`
+	End   string `json:"end"`
+	Text  string `json:"text"`
+}
