513aa6e9be35ccd416ff6765d3826fe7633fc06e
B33Boy <abhip202@gmail.com>
2025-06-05 14:20:11 -0400

    fix vtt file dir

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -11,8 +11,12 @@ func ExtractCaptions(url string, outputDir string) (*CaptionMetadata, error) {
 
 	// Run yt-dlp to download vtt file
 	captionMetadata, err := getMetadata(url, outputDir)
+	if err != nil {
+		return nil, fmt.Errorf("getMetadata failed: %w", err)
+	}
 
-	err = getCaptions(captionMetadata)
+	vttCaptionsDir := "tmp/captions/"
+	err = getCaptions(vttCaptionsDir, captionMetadata)
 	if err != nil {
 		return nil, fmt.Errorf("getCaptions failed: %w", err)
 	}
@@ -20,7 +24,7 @@ func ExtractCaptions(url string, outputDir string) (*CaptionMetadata, error) {
 	log.Println("Downloaded vtt file for Id:", captionMetadata.VideoId)
 
 	// Run scripts/extract_captions.py to convert to a JSON format
-	cmd := exec.Command("./venv/Scripts/python", "scripts/extract_captions.py", getVttFileNameFromId(captionMetadata.VideoId))
+	cmd := exec.Command("./venv/Scripts/python", "scripts/extract_captions.py", vttCaptionsDir+captionMetadata.VideoId+".en.vtt")
 	cmdOutput, err := cmd.CombinedOutput()
 	if err != nil {
 		return nil, fmt.Errorf("failed to run extract_captions.py: %w\nOutput: %s", err, cmdOutput)
@@ -31,9 +35,9 @@ func ExtractCaptions(url string, outputDir string) (*CaptionMetadata, error) {
 	return captionMetadata, nil
 }
 
-func getCaptions(metadata *CaptionMetadata) error {
+func getCaptions(vttCaptionsDir string, metadata *CaptionMetadata) error {
 
-	cmd := exec.Command("yt-dlp.exe", "--write-subs", "--write-auto-subs", "--no-warnings", "--sub-langs", "en", "--skip-download", metadata.Url, "-o", metadata.CaptionPath)
+	cmd := exec.Command("yt-dlp.exe", "--write-subs", "--write-auto-subs", "--no-warnings", "--sub-langs", "en", "--skip-download", metadata.Url, "-o", vttCaptionsDir+metadata.VideoId)
 
 	cmdOutput, err := cmd.CombinedOutput()
 
@@ -50,22 +54,19 @@ func getMetadata(url string, path string) (*CaptionMetadata, error) {
 
 	raw, err := exec.Command("yt-dlp.exe", "--get-id", "--get-title", "--no-warnings", "--skip-download", url).Output()
 	if err != nil {
-		return "", "", fmt.Errorf("unable to fetch video Id")
+		return nil, fmt.Errorf("unable to fetch video metadata")
 	}
 
-	parts := bytes.Split(raw, []byte("\n"), 2)
+	parts := bytes.Split(raw, []byte("\n"))
 	if len(parts) < 2 {
-		return nil, fmt.Errorf("unable to download video captions: %w\nOutput: %s", err, cmdOutput)
+		return nil, fmt.Errorf("unable to parse the metadata")
 	}
 
-	id := string(bytes.TrimSpace(parts[0]))
-	title := string(bytes.TrimSpace(parts[1]))
+	// Order matters for yt-dlp as it always outputs title then id
+	title := string(bytes.TrimSpace(parts[0]))
+	id := string(bytes.TrimSpace(parts[1]))
 
-	metadata := CaptionMetadata{VideoId: id, VideoTitle: title, Url: url, CaptionPath: path + id + "en.json"}
+	metadata := CaptionMetadata{VideoId: id, VideoTitle: title, Url: url, CaptionPath: path + id + ".en.json"}
 
 	return &metadata, nil
-}
-
-func getVttFileNameFromId(id string) string {
-	return "tmp/captions/" + id + ".en.vtt"
 }
