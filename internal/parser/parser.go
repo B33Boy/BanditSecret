590ad336f527fcf3cb6f5800d26337c3d86f25b1
B33Boy <abhip202@gmail.com>
2025-06-10 18:59:57 -0400

    refactor: add parser to load json to structs

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -1,106 +1,115 @@
 package parser
 
 import (
-	"bytes"
+	"encoding/json"
 	"fmt"
-	"log"
 	"os"
-	"os/exec"
-	"strings"
-	"time"
 )
 
-func fileExists(path string) bool {
-	_, err := os.Stat(path)
-	return err == nil || !os.IsNotExist(err)
+type Parser interface {
+	ParseJSON(jsonFile string) ([]CaptionEntry, error)
 }
 
-func ExtractCaptions(url string, outputDir string) (*CaptionMetadata, error) {
+type ParserService struct{}
 
-	// Run yt-dlp to download vtt file
-	captionMetadata, err := getMetadata(url, outputDir)
-	if err != nil {
-		return nil, fmt.Errorf("getMetadata failed: %w", err)
-	}
-
-	// Don't download if captions file already exists
-	vttCaptionsDir := "tmp/captions/"
-	vttCaptionsFile := vttCaptionsDir + captionMetadata.VideoId + ".en.vtt"
-
-	if !fileExists(vttCaptionsFile) {
-		err = getCaptions(vttCaptionsDir, captionMetadata)
-		if err != nil {
-			return nil, fmt.Errorf("getCaptions failed: %w", err)
-		}
-
-		log.Println("Downloaded vtt file for Id:", captionMetadata.VideoId)
-	} else {
-		log.Printf("Vtt file %s already exists! Skipping download\n", vttCaptionsFile)
-	}
-
-	// Run scripts/extract_captions.py to convert to a JSON format
-	cmd := exec.Command("./venv/Scripts/python", "scripts/extract_captions.py", vttCaptionsFile)
-	cmdOutput, err := cmd.CombinedOutput()
-	if err != nil {
-		return nil, fmt.Errorf("failed to run extract_captions.py: %w\nOutput: %s", err, cmdOutput)
-	}
-
-	log.Println("Done extracting")
-
-	return captionMetadata, nil
+func NewParserService() *ParserService {
+	return &ParserService{}
 }
 
-func getCaptions(vttCaptionsDir string, metadata *CaptionMetadata) error {
+func (s *ParserService) ParseJSON(jsonFile string) ([]CaptionEntry, error) {
 
-	cmd := exec.Command("yt-dlp.exe", "--write-subs", "--write-auto-subs", "--no-warnings", "--sub-langs", "en", "--skip-download", metadata.Url, "-o", vttCaptionsDir+metadata.VideoId)
-
-	cmdOutput, err := cmd.CombinedOutput()
-
-	if err != nil {
-		return fmt.Errorf("unable to download video captions: %w\nOutput: %s", err, cmdOutput)
-	} else {
-		log.Println(string(cmdOutput))
+	_, err := os.Stat(jsonFile)
+	if os.IsNotExist(err) {
+		return nil, fmt.Errorf("JSON caption file not found at %s: %w", jsonFile, err)
 	}
 
-	return nil
+	data, err := os.ReadFile(jsonFile)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read JSON file %s: %w", jsonFile, err)
+	}
+
+	var captions []CaptionEntry
+
+	err = json.Unmarshal(data, &captions)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal JSON data from %s: %w", jsonFile, err)
+	}
+	return captions, nil
 }
 
-func getMetadata(url string, path string) (*CaptionMetadata, error) {
+// =================================================================================================
+// func fileExists(path string) bool {
+// 	_, err := os.Stat(path)
+// 	return os.IsNotExist(err)
+// }
 
-	raw, err := exec.Command("yt-dlp.exe", "--get-id", "--get-title", "--no-warnings", "--skip-download", url).Output()
-	if err != nil {
-		return nil, fmt.Errorf("unable to fetch video metadata")
-	}
+// func ExtractCaptions(url string, outputDir string) (*CaptionMetadata, error) {
 
-	parts := bytes.Split(raw, []byte("\n"))
-	if len(parts) < 2 {
-		return nil, fmt.Errorf("unable to parse the metadata")
-	}
+// 	// Run yt-dlp to download vtt file
+// 	captionMetadata, err := getMetadata(url, outputDir)
+// 	if err != nil {
+// 		return nil, fmt.Errorf("getMetadata failed: %w", err)
+// 	}
 
-	// Order matters for yt-dlp as it always outputs title then id
-	title := string(bytes.TrimSpace(parts[0]))
-	id := string(bytes.TrimSpace(parts[1]))
+// 	// Don't download if captions file already exists
+// 	vttCaptionsDir := "tmp/captions/"
+// 	vttCaptionsFile := vttCaptionsDir + captionMetadata.VideoId + ".en.vtt"
 
-	metadata := CaptionMetadata{VideoId: id, VideoTitle: title, Url: url, CaptionPath: path + id + ".en.json"}
+// 	if !fileExists(vttCaptionsFile) {
+// 		err = getCaptions(vttCaptionsDir, captionMetadata)
+// 		if err != nil {
+// 			return nil, fmt.Errorf("getCaptions failed: %w", err)
+// 		}
 
-	return &metadata, nil
-}
+// 		log.Println("Downloaded vtt file for Id:", captionMetadata.VideoId)
+// 	} else {
+// 		log.Printf("Vtt file %s already exists! Skipping download\n", vttCaptionsFile)
+// 	}
 
-func (t *TimeMs) UnmarshalJSON(data []byte) error {
+// 	// Run scripts/extract_captions.py to convert to a JSON format
+// 	cmd := exec.Command("./venv/Scripts/python", "scripts/extract_captions.py", vttCaptionsFile)
+// 	cmdOutput, err := cmd.CombinedOutput()
+// 	if err != nil {
+// 		return nil, fmt.Errorf("failed to run extract_captions.py: %w\nOutput: %s", err, cmdOutput)
+// 	}
 
-	s := strings.Trim(string(data), `"`)
+// 	log.Println("Done extracting")
 
-	parsedTime, err := time.Parse("15:04:05.000", s)
-	if err != nil {
-		return err
-	}
+// 	return captionMetadata, nil
+// }
 
-	hours := parsedTime.Hour() * 60 * 60 * 1000
-	mins := parsedTime.Minute() * 60 * 1000
-	secs := parsedTime.Second() * 1000
-	msecs := parsedTime.Nanosecond() / 1e6
+// func getCaptions(vttCaptionsDir string, metadata *CaptionMetadata) error {
 
-	*t = TimeMs(hours + mins + secs + msecs)
+// 	cmd := exec.Command("yt-dlp.exe", "--write-subs", "--write-auto-subs", "--no-warnings", "--sub-langs", "en", "--skip-download", metadata.Url, "-o", vttCaptionsDir+metadata.VideoId)
 
-	return nil
-}
+// 	cmdOutput, err := cmd.CombinedOutput()
+
+// 	if err != nil {
+// 		return fmt.Errorf("unable to download video captions: %w\nOutput: %s", err, cmdOutput)
+// 	} else {
+// 		log.Println(string(cmdOutput))
+// 	}
+
+// 	return nil
+// }
+
+// func getMetadata(url string, path string) (*CaptionMetadata, error) {
+
+// 	raw, err := exec.Command("yt-dlp.exe", "--get-id", "--get-title", "--no-warnings", "--skip-download", url).Output()
+// 	if err != nil {
+// 		return nil, fmt.Errorf("unable to fetch video metadata")
+// 	}
+
+// 	parts := bytes.Split(raw, []byte("\n"))
+// 	if len(parts) < 2 {
+// 		return nil, fmt.Errorf("unable to parse the metadata")
+// 	}
+
+// 	// Order matters for yt-dlp as it always outputs title then id
+// 	title := string(bytes.TrimSpace(parts[0]))
+// 	id := string(bytes.TrimSpace(parts[1]))
+
+// 	metadata := CaptionMetadata{VideoId: id, VideoTitle: title, Url: url, CaptionPath: path + id + ".en.json"}
+
+// 	return &metadata, nil
+// }
--- internal/parser/types.go
+++ internal/parser/types.go
@@ -1,17 +1,33 @@
 package parser
 
+import (
+	"strings"
+	"time"
+)
+
 type TimeMs uint32
-
-type CaptionMetadata struct {
-	VideoId     string
-	VideoTitle  string
-	Url         string
-	CaptionPath string
-}
-
 type CaptionEntry struct {
 	VideoId string `json:"video_id"`
 	Start   TimeMs `json:"start"`
 	End     TimeMs `json:"end"`
 	Text    string `json:"text"`
+}
+
+func (t *TimeMs) UnmarshalJSON(data []byte) error {
+
+	s := strings.Trim(string(data), `"`)
+
+	parsedTime, err := time.Parse("15:04:05.000", s)
+	if err != nil {
+		return err
+	}
+
+	hours := parsedTime.Hour() * 60 * 60 * 1000
+	mins := parsedTime.Minute() * 60 * 1000
+	secs := parsedTime.Second() * 1000
+	msecs := parsedTime.Nanosecond() / 1e6
+
+	*t = TimeMs(hours + mins + secs + msecs)
+
+	return nil
 }
