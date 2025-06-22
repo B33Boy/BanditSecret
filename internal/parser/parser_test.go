6218c845956d0215654254b11667ef48cf13050c
B33Boy <abhip202@gmail.com>
2025-06-21 23:48:21 -0400

    chore: add more tests for ParseJSON

--- internal/parser/parser_test.go
+++ internal/parser/parser_test.go
@@ -1,6 +1,8 @@
 package parser
 
 import (
+	"fmt"
+	"strings"
 	"testing"
 )
 
@@ -20,9 +22,10 @@ func TestParseJSON(t *testing.T) {
 			Text:    "SampleText2",
 		},
 	}
-
+	inpFile := "testdata/sample.json"
 	parserService := NewParserService()
-	got, err := parserService.ParseJSON("testdata/sample.json")
+
+	got, err := parserService.ParseJSON(inpFile)
 	if err != nil {
 		t.Fatalf("ParseJSON failed: %v", err)
 	}
@@ -36,5 +39,35 @@ func TestParseJSON(t *testing.T) {
 			t.Fatalf("ParseJSON failed: parsed entry does not match expected:\nreceived: %+v\nexpected: %+v", entry, want[i])
 		}
 	}
+}
 
+func TestParseJSONFileDoesNotExist(t *testing.T) {
+	inpFile := "testdata/does_not_exist.json"
+	parserService := NewParserService()
+
+	_, err := parserService.ParseJSON(inpFile)
+	if err == nil {
+		t.Fatalf("TestParseJSONFileDoesNotExist failed: expected an error, got nil")
+	}
+
+	expected := fmt.Sprintf("JSON caption file not found at %s", inpFile)
+	if !strings.Contains(err.Error(), expected) {
+		t.Fatalf("TestParseJSONFileDoesNotExist failed: expected error to contain %s, but got %s", expected, err.Error())
+	}
+}
+
+func TestParseJSONFileCannotBeUnmarshalled(t *testing.T) {
+	inpFile := "testdata/badsample.json"
+	parserService := NewParserService()
+
+	_, err := parserService.ParseJSON(inpFile)
+	if err == nil {
+		t.Fatalf("TestParseJSONFileCannotBeUnmarshalled failed: expected an error, got nil")
+	}
+
+	expected := fmt.Sprintf("failed to unmarshal JSON data from %s: json: cannot unmarshal object into Go value of type []parser.CaptionEntry", inpFile)
+
+	if !strings.Contains(err.Error(), expected) {
+		t.Fatalf("TestParseJSONFileDoesNotExist failed: expected error to contain \"%s\", but got \"%s\"", expected, err.Error())
+	}
 }
